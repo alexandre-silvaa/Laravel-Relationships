6fca0ff423b3b1158dbb2e32bdb6cee3edb8c2f3
alexadre-silvaa <alexandrebsilva9@gmail.com>
2022-05-16 17:19:55 -0300

    Criando tabelas One to Many - Laravel

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,11 @@ Route::get('one-to-one', 'OneToOneController@oneToOne');
 Route::get('one-to-one-inverse', 'OneToOneController@oneToOneInverse');
 Route::get('one-to-one-insert', 'OneToOneController@oneToOneInsert');
 
+/*
+One to Many
+ */
+Route::get('one-to-many', 'OneToManyController@oneToMany');
+
 
 
 Route::get('/', function () {
