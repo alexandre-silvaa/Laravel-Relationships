c48ee4fc8ae029da9358a70688dc4b6f9d8ab96d
alexadre-silvaa <alexandrebsilva9@gmail.com>
2022-05-23 14:13:44 -0300

    Relation Polymorph

--- routes/web.php
+++ routes/web.php
@@ -11,34 +11,31 @@
 |
 */
 
-/*
-One to One
- */
+//One to One
 Route::get('one-to-one', 'OneToOneController@oneToOne');
 Route::get('one-to-one-inverse', 'OneToOneController@oneToOneInverse');
 Route::get('one-to-one-insert', 'OneToOneController@oneToOneInsert');
 
-/*
-One to Many
- */
+//One to Many
 Route::get('one-to-many', 'OneToManyController@oneToMany');
 Route::get('many-to-one', 'OneToManyController@manyToOne');
 Route::get('one-to-many-two', 'OneToManyController@oneToManyTwo');
 Route::get('one-to-many-insert', 'OneToManyController@oneToManyInsert');
 Route::get('one-to-many-insert-two', 'OneToManyController@oneToManyInsertTwo');
 
-/*
-* Has many Through
- */
+
+//Has many Through
 Route::get('has-many-through', 'OneToManyController@hasManyThrough');
 
-/*
-* Many to Many
- */
+//Many to Many
 Route::get('many-to-many', 'ManyToManyController@manyToMany');
 Route::get('many-to-many-inverse', 'ManyToManyController@manyToManyInverse');
 Route::get('many-to-many-insert', 'ManyToManyController@manyToManyInsert');
 
+//Relation Polymorphic
+Route::get('polymorphics', 'PolymorphicController@polymorphic');
+
+//Default
 Route::get('/', function () {
     return view('welcome');
 });
