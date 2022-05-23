ec52061ee91c0cb361989ffcdf3d8a0b0c9733a3
alexadre-silvaa <alexandrebsilva9@gmail.com>
2022-05-23 12:10:29 -0300

    Many to Many Insert - Laravel

--- app/Http/Controllers/ManyToManyController.php
+++ app/Http/Controllers/ManyToManyController.php
@@ -30,4 +30,23 @@ class ManyToManyController extends Controller
         }
     }
 
+    public function manyToManyInsert()
+    {
+        //$dataForm = [3,4,5];
+        $dataForm = [3,4];
+
+        $company = Company::find(1);
+        echo "<b> {$company->name} </br></b>";
+
+        //$company->cities()->attach($dataForm);
+        //$company->cities()->sync($dataForm);
+        $company->cities()->detach($dataForm);
+
+        $cities = $company->cities;
+        foreach($cities as $city){
+            echo " {$city->name}, ";
+        }
+
+    }
+
 }
